49468f9d958256e3877a457227ec01f0910d46b9
Birabhadra <birabhadrasaho019@gmail.com>
2026-08-23 15:59:51 +0530

    feat:suffix stage

--- src/services/contextGatherer.ts
+++ src/services/contextGatherer.ts
@@ -4,24 +4,28 @@ import { PrefixStage } from "./contextStages/prefixStage";
 import { LspService } from "./lspService";
 import { ReplacementRegionStage } from "./contextStages/replacementRegionStage";
 import { ASTService } from "./astService";
+import { SuffixStage } from "./contextStages/suffixStage";
 export class ContextGatherer implements vscode.Disposable {
     private readonly intentTracker: IntentTracker;
     private readonly prefixStage: PrefixStage;
+    private readonly suffixStage: SuffixStage;
     private readonly lspService: LspService;
     private readonly replacementRegionStage:ReplacementRegionStage;
     constructor(astService:ASTService,intentTracker: IntentTracker) {
         this.intentTracker = intentTracker;
         this.lspService = new LspService();
         this.prefixStage = new PrefixStage(this.lspService);
+        this.suffixStage=new SuffixStage();
         this.replacementRegionStage=new ReplacementRegionStage(astService)
     }
 
     async gatherContext(document: vscode.TextDocument, position: vscode.Position): Promise<string> {
         const replacementRegion=this.replacementRegionStage.compute(document,position)
         const prefix= await this.prefixStage.buildPrefix(document, position) ?? '';
+        const suffix=this.suffixStage.buildSuffixAfterRegion(document,replacementRegion.range.end) ?? '';
         const editHistory = this.intentTracker.serialize()
         
-        return replacementRegion.text;
+        return suffix;
     }
     dispose(): void {
         //no-operation for now
