a80c766423a2e3f062aade13b6886fe932a65038
Birabhadra <birabhadrasaho019@gmail.com>
2026-08-29 09:58:00 +0530

    ts7 upgrade and format fix

---
+++ scripts/eslint-ts6-loader.cjs
@@ -0,0 +1,11 @@
+const Module = require('node:module');
+
+const originalResolveFilename = Module._resolveFilename;
+
+Module._resolveFilename = function patchedResolveFilename(request, parent, isMain, options) {
+    if (request === 'typescript') {
+        return originalResolveFilename.call(this, 'typescript6', parent, isMain, options);
+    }
+
+    return originalResolveFilename.call(this, request, parent, isMain, options);
+};
